03e034e1e10c899390a5c6b1e1b7c9648254a32d
Hossain-Altaf <altaf.swe13@gmail.com>
2026-09-10 03:54:35 +0600

    live

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://betaar.vercel.app'],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
